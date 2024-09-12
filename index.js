d1583de4e75dc81ddd7f47d146453232f1b015e3
Thaissa Santana <thaissasantana.dev@gmail.com>
2024-09-12 07:45:23 -0300

    fundamentos 002

---
+++ index.js
@@ -0,0 +1,21 @@
+// hello world
+
+console.log("Hello World!");
+
+// arrays, objetos
+
+let metas = ["Mayk", "Alô"];
+console.log(metas[1] + " " + metas[0]);
+
+let meta = {
+    value: 'Ler um livro por mês',
+    checked: false,
+    log: (info) => {console.log (info)}   
+}
+
+meta.log(meta.value);
+
+// function // arrow function
+
+// const criarMeta = () => {}
+function criarMeta () {}
