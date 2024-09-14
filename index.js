c59399a1229dfb476e5a18be4dc47a0d496edef4
Thaissa Santana <thaissasantana.dev@gmail.com>
2024-09-13 21:55:17 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -59,10 +59,25 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas: " + realizadas.length,
         choices: [...realizadas]
+    })  
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return !meta.checked                                                 //poderia ser assim- meta.checked != true
     })
-    
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return 
+    }
+
+    await select({
+        message: "Metas Abertas: " + abertas.length,
+        choices: [...abertas]
+    })
 }
 
 const start = async () => {
@@ -85,6 +100,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -102,6 +121,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!");
